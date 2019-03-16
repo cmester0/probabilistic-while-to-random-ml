56f73e2de21b192a56cc0f6428cf5a0cb5d3defe
Lasse <lasse@letager.dk>
2019-03-16 22:31:14 +0100

    Updated to fit with Rml changes

--- pWhileInterp.v
+++ pWhileInterp.v
@@ -6,28 +6,22 @@ Require Import Rml.
 
 From mathcomp.analysis Require Import boolp reals distr.
 
-Check @cmd.
-
-Check (vars_ _) _.
-
 Inductive well_defined_pWhile : cmd -> Prop :=
 | well_def_skip : well_defined_pWhile skip
 | well_def_abort : well_defined_pWhile skip
 | well_def_assign : forall {t} (x : (vars_ _) t) e, well_defined_pWhile (assign x e).
 
-Definition initial_env : ident -> nat :=
-  fun y => 0. (* Return is always 0 *)
+Definition initial_env p : ident -> (nat * Type) :=
+  fun y => p. (* Return is always (0,Set), make better default (return var name) *)
 
 Check ident.
 
-Fixpoint extend_env (x : ident) (v : nat) (env : ident -> nat) : ident -> nat :=
+Fixpoint extend_env (x : ident) (v : (nat * Type)) (env : ident -> (nat * Type)) : ident -> (nat * Type) :=
   fun y =>
     if pselect (y = x)
     (* @mget _ k (x%V).(vtype) m (x%V).(vname) == @mget _ _ (y%V).(vtype) m (y%V).(vname) *)
     then v
     else env y.
-
-Check ident.
 
 Theorem get_set :
   forall m x v, (@extend_env x v m) x = v.
@@ -40,15 +34,9 @@ Proof.
       reflexivity.
     + intros.
       easy.
-  - simpl.
-    case (pselect _).
-    + intros.
-      reflexivity.
-    + intros.
-      easy.
 Qed.
 
-Fixpoint translate_pWhile_expr_to_rml {T} (x : expr T) (env : ident -> nat) :=
+Fixpoint translate_pWhile_expr_to_rml {T} (x : expr T) (env : ident -> (nat * Type)) :=
   match x with
   | var_ A n =>
     Var (env n.(vname))
@@ -58,7 +46,7 @@ Fixpoint translate_pWhile_expr_to_rml {T} (x : expr T) (env : ident -> nat) :=
   end.  
 
 Example translate_exp_cst :
-    forall n, translate_pWhile_expr_to_rml (cst_ n) (initial_env) = Const nat n.
+    forall n p, translate_pWhile_expr_to_rml (cst_ n) (initial_env p) = Const nat n.
 Proof.
   intros.
   simpl.
@@ -67,7 +55,7 @@ Proof.
 Qed.
 
 Example translate_exp_var :
-    forall T x n, translate_pWhile_expr_to_rml (var_ x) (@extend_env (@vname _ T x) n initial_env) = Var n.
+    forall T x n p, translate_pWhile_expr_to_rml (var_ x) (@extend_env (@vname _ T x) n (initial_env p)) = Var n.
 Proof.
   intros.
   simpl.
@@ -79,16 +67,9 @@ Proof.
       reflexivity.
     + intros.
       easy.
-  - simpl.
-    case (pselect _).
-    + intros.
-      simpl.
-      reflexivity.
-    + intros.
-      easy.
 Qed.
 
-Fixpoint translate_pWhile_cmd_to_rml (x : cmd) {T} (ret : vars T) (env : ident -> nat) : Rml :=
+Fixpoint translate_pWhile_cmd_to_rml (x : cmd) {T} (ret : vars T) (env : ident -> (nat * Type)) : Rml :=
   match x with
   | seqc (assign A n e) e2 => Let_stm (env n.(vname)) (translate_pWhile_expr_to_rml e (@env)) (translate_pWhile_cmd_to_rml e2 ret env)
     
@@ -96,15 +77,20 @@ Fixpoint translate_pWhile_cmd_to_rml (x : cmd) {T} (ret : vars T) (env : ident -
   | skip => Var (env ret.(vname))
   | assign A n e => Let_stm (env n.(vname)) (translate_pWhile_expr_to_rml e (@env)) (Var (env ret.(vname)))
   | random A n e => Var (env ret.(vname))
-  | cond b m1 m2 => If_stm (translate_pWhile_cmd_to_rml b) (translate_pWhile_cmd_to_rml b) (translate_pWhile_cmd_to_rml b) 
+  | cond b m1 m2 => If_stm (translate_pWhile_expr_to_rml b (@env)) (translate_pWhile_cmd_to_rml m1 ret (@env)) (translate_pWhile_cmd_to_rml m2 ret (@env)) 
   | while b e => Var (env ret.(vname))
-  | seqc e1 e2 => Let_stm 999 (translate_pWhile_cmd_to_rml e1 ret env) (translate_pWhile_cmd_to_rml e2 ret env)
+  | seqc e1 e2 => Let_stm (999,Set) (translate_pWhile_cmd_to_rml e1 ret env) (translate_pWhile_cmd_to_rml e2 ret env)
   end.
 
-Example translate_cmd_cst :
-    forall x (n1 n2 : nat), translate_pWhile_cmd_to_rml (seqc (assign x (cst_ n1)) (assign x (cst_ n2))) x (@initial_env) = Let_stm 0 (Const nat n1) (Let_stm 0 (Const nat n2) (Var 0)).
+Example translate_cmd_example1 :
+  forall (T : Type) x (n1 n2 : nat),
+    T = nat ->
+    translate_pWhile_cmd_to_rml
+      (seqc (assign x (cst_ n1)) (assign x (cst_ n2))) x (@initial_env (0,T))
+    = Let_stm (0,T) (Const nat n1) (Let_stm (0,T) (Const nat n2) (Var (0,T))).
 Proof.
   intros.
+  subst.
   simpl.
   unfold initial_env.
   reflexivity.
